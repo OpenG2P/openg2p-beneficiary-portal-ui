c6b3442e5e1cdc08cfa956935dfe96f226d1b3c6
vin0dkhichar <abbb61667@gmail.com>
2025-10-29 10:18:38 +0530

    G2P-3482 Added pagination and search for news

--- src/features/news/utils/getNews.ts
+++ src/features/news/utils/getNews.ts
@@ -1,14 +1,31 @@
 import qs from "qs";
 import { News } from "@/features/news/types";
 
-export async function getNews(): Promise<News[]> {
+export async function getNews(
+    page: number = 1,
+    pageSize: number = 5,
+    search: string = ""
+): Promise<{ data: News[]; total: number }> {
+
     const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:1337";
     const path = "/api/allnews";
-
     const url = new URL(path, baseUrl);
 
     url.search = qs.stringify(
         {
+            pagination: {
+                page,
+                pageSize,
+            },
+            sort: ["createdAt:desc"],
+            filters: search
+                ? {
+                    $or: [
+                        { title: { $containsi: search } },
+                        { description: { $containsi: search } },
+                    ],
+                }
+                : undefined,
             populate: {
                 image: {
                     fields: ["url", "alternativeText"],
@@ -19,21 +36,23 @@ export async function getNews(): Promise<News[]> {
     );
 
     const res = await fetch(url.toString(), { cache: "no-cache" });
-
-    if (!res.ok) throw new Error("Failed to fetch notifications");
+    if (!res.ok) throw new Error("Failed to fetch news");
 
     const json = await res.json();
 
-    return json.data.map((item: any) => ({
-        id: item.id,
-        title: item.title,
-        description: item.description,
-        date: item.date,
-        image: item.image
-            ? {
-                url: item.image.url,
-                alternativeText: item.image.alternativeText ?? "",
-            }
-            : undefined,
-    }));
+    return {
+        data: json.data.map((item: any) => ({
+            id: item.id,
+            title: item.title,
+            description: item.description,
+            date: item.date,
+            image: item.image
+                ? {
+                    url: item.image.url,
+                    alternativeText: item.image.alternativeText ?? "",
+                }
+                : undefined,
+        })),
+        total: json.meta.pagination.total,
+    };
 }

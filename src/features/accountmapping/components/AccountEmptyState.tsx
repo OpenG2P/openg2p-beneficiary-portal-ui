8b392cc79152093904e7e09b38cf532e59f04946
vin0dkhichar <abbb61667@gmail.com>
2025-11-17 10:33:20 +0530

    Fix landing page alignment

--- src/features/accountmapping/components/AccountEmptyState.tsx
+++ src/features/accountmapping/components/AccountEmptyState.tsx
@@ -12,7 +12,7 @@ export default function AccountEmptyState({ lang }: { lang: string }) {
             </h1>
 
             <div className="flex justify-center items-center">
-                <div className="w-full min-h-[75vh] flex flex-col justify-center items-center text-center bg-white p-8 rounded-[10px] shadow-[0_4px_20px_rgba(0,0,0,0.25)]">
+                <div className="w-full min-h-[75vh] flex flex-col justify-center items-center text-center bg-white p-8 rounded-[10px] drop-shadow-[0_4px_20px_rgba(0,0,0,0.25)]">
                     <h2 className="text-[22px] font-[600] text-gray-800 mb-4">
                         No Linked Account Found
                     </h2>
